1d599979a6197faa469e135fe50659e49dfcc5cc
Norbert Horgas <norbert.horgas@mathema.de>
2021-04-08 11:52:12 +0200

    Add loading indicator to vacuum controls

--- client/src/routes/home/VacuumControls.tsx
+++ client/src/routes/home/VacuumControls.tsx
@@ -1,34 +1,61 @@
 import React from "react";
 
-import { Button, Center, Grid, Stack } from "@chakra-ui/react";
+import { Button, Center, Grid, Progress, Stack } from "@chakra-ui/react";
 
 import { useVacuum } from "../../service/useVacuum";
 import { RoomList } from "../../types/maps";
 
 export const VacuumControls = () => {
-  const { startRoomCleaning, pause, home } = useVacuum();
+  const {
+    startRoomCleaning,
+    sendingRoomRequest,
+    sendingActionRequest,
+    pause,
+    home
+  } = useVacuum();
 
   return (
-    <Center>
-      <Stack>
-        <Grid templateColumns="repeat(2, 1fr)" gap={6} my="3">
-          {RoomList.map((room) => (
-            <Button
-              key={room}
-              colorScheme="teal"
-              onClick={() => startRoomCleaning(room)}
-            >
-              {room}
-            </Button>
-          ))}
-        </Grid>
-        <Button colorScheme="facebook" onClick={pause}>
-          Pause
-        </Button>
-        <Button colorScheme="facebook" onClick={home}>
-          Go Home
-        </Button>
-      </Stack>
-    </Center>
+    <>
+      <Center position="relative">
+        {(sendingRoomRequest || sendingActionRequest) && (
+          <Progress
+            size="sm"
+            isIndeterminate
+            position="absolute"
+            top="0"
+            left="0"
+            w="100%"
+          />
+        )}
+        <Stack>
+          <Grid templateColumns="repeat(2, 1fr)" gap={6} my="5">
+            {RoomList.map((room) => (
+              <Button
+                disabled={sendingRoomRequest}
+                key={room}
+                colorScheme="teal"
+                onClick={() => startRoomCleaning(room)}
+              >
+                {room}
+              </Button>
+            ))}
+          </Grid>
+          <Button
+            colorScheme="facebook"
+            onClick={pause}
+            disabled={sendingActionRequest}
+          >
+            Pause
+          </Button>
+          <Button
+            colorScheme="facebook"
+            onClick={home}
+            disabled={sendingActionRequest}
+          >
+            Go Home
+          </Button>
+        </Stack>
+      </Center>
+    </>
   );
 };
--- client/src/service/useVacuum.ts
+++ client/src/service/useVacuum.ts
@@ -7,6 +7,8 @@ import { StatusProps } from "../types/status-response";
 
 export const useVacuum = () => {
   const [status, setStatus] = useState<StatusProps>();
+  const [sendingRoomRequest, setSendingRoomRequest] = useState(false);
+  const [sendingActionRequest, setSendingActionRequest] = useState(false);
   const [statusLoading, setStatusLoading] = useState(false);
   const toast = useToast();
 
@@ -18,42 +20,59 @@ export const useVacuum = () => {
   };
 
   const startRoomCleaning = async (room: string) => {
-    const { Response } = await VacuumEndpoints.startRoomCleaning(room);
-    toast({
-      title: "Room Cleaning",
-      description: Response,
-      status: "success",
-      duration: 9000,
-      isClosable: true
-    });
+    try {
+      setSendingRoomRequest(true);
+      const { Response } = await VacuumEndpoints.startRoomCleaning(room);
+      toast({
+        title: "Room Cleaning",
+        description: Response,
+        status: "success",
+        duration: 9000,
+        isClosable: true
+      });
+    } finally {
+      setSendingRoomRequest(false);
+    }
   };
 
   const pause = async () => {
-    const { Response } = await VacuumEndpoints.pause();
-    toast({
-      title: "Room Cleaning",
-      description: Response,
-      status: "success",
-      duration: 9000,
-      isClosable: true
-    });
+    try {
+      setSendingActionRequest(true);
+      const { Response } = await VacuumEndpoints.pause();
+      toast({
+        title: "Room Cleaning",
+        description: Response,
+        status: "success",
+        duration: 9000,
+        isClosable: true
+      });
+    } finally {
+      setSendingActionRequest(false);
+    }
   };
 
   const home = async () => {
-    const { Response } = await VacuumEndpoints.home();
-    toast({
-      title: "Room Cleaning",
-      description: Response,
-      status: "success",
-      duration: 9000,
-      isClosable: true
-    });
+    try {
+      setSendingActionRequest(true);
+      const { Response } = await VacuumEndpoints.home();
+      toast({
+        title: "Room Cleaning",
+        description: Response,
+        status: "success",
+        duration: 9000,
+        isClosable: true
+      });
+    } finally {
+      setSendingActionRequest(false);
+    }
   };
 
   return {
     status,
     getStatus,
     statusLoading,
+    sendingRoomRequest,
+    sendingActionRequest,
     startRoomCleaning,
     pause,
     home
